247b86fb42fe23968206cb208666d68dc4fd8ebc
Jixu Ni <jixuni@Jixus-MBP.home>
2019-01-02 13:57:37 -0500

    added server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+
+app.listen(PORT), () => {
+    console.log("App listening on port " + PORT);
+}
