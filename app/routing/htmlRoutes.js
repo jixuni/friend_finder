143d806f92542eeb5a25390426424e420f6a9388
Jixu Ni <jixuni@Jixus-MBP.home>
2019-01-05 12:55:47 -0500

    fix some errors

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+router.get("/survey", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,14 +9,6 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", (req, res) => {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
 
 app.listen(PORT, () => {
     console.log("App listening on port " + PORT);
